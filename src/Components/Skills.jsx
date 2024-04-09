683d208ae491de07cc864403a0b0b1baeef14737
Nishchay SK <sk.nishchay@gmail.com>
2024-04-10 00:56:13 +0530

    personal portfolio

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,40 +1,60 @@
 import React from "react"
+import Go from "../assets/go.svg"
+import Java from "../assets/java.svg"
+import JS from "../assets/js.svg"
+import ReactJS from "../assets/reactjs.svg"
+import HTML from "../assets/html.svg"
+import CSS from "../assets/css.svg"
+import Docker from "../assets/docker.svg"
+import Kubernetes from "../assets/kubernetes.svg"
+import MySQL from "../assets/mysql.svg"
+import MongoDB from "../assets/mongodb.svg"
 
 const Skills = () => {
 
     const skills = [
         {
             skill: "GO",
+            logo: Go,
         },
         {
             skill: "Java",
+            logo: Java,
         },
         {
             skill: "JavaScript",
+            logo: JS,
         },
         {
             skill: "ReactJs",
+            logo: ReactJS,
         },
         {
             skill: "HTML",
+            logo: HTML,
         },
         {
             skill: "CSS",
+            logo: CSS,
         },
         {
             skill: "Docker",
+            logo: Docker,
         },
         {
             skill: "Kubernetes",
-        },
-        {
-            skill: "Microservices",
+            logo: Kubernetes,
         },
         {
             skill: "MySQL",
+            logo: MySQL,
         },
         {
             skill: "MongoDB",
+            logo: MongoDB,
+        },
+        {
+            skill: "Microservices",
         },
         {
             skill: "Data Structures"
@@ -54,8 +74,8 @@ const Skills = () => {
             {
                 skills.map((skill) => {
                     return (
-                        <div className="skill-logo">
-                            {skill.skill}
+                        <div className="skill-container">
+                            {skill.logo ? <img className="skill-logo" src={skill.logo} alt={skill.logo} /> : skill.skill}
                         </div>
                     )
                 })
