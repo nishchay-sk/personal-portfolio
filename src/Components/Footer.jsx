5bd8e1b3d5ba04d6aaa085de88c3be39f90b62c1
Nishchay SK <sk.nishchay@gmail.com>
2024-04-11 20:39:23 +0530

    personal portfolio

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import Heart from "../assets/heart.svg";
 const Footer = () => {
     return (
         <div id="footer">
-            <p id="text">Made with{<img src={Heart} alt="Love" />}by<strong>{`${'<Nishchay SK />'}`}</strong></p>
+            <p id="text">Developed with{<img src={Heart} alt="Love" />}by<strong>{`${'<Nishchay SK />'}`}</strong></p>
         </div>
     )
 }
